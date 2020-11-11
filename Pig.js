ef30b2ad4a12d3ccdefb7677b113e79e2279e8ce
riynig125 <71161480+riynig125@users.noreply.github.com>
2020-11-10 19:07:02 -0800

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,31 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.visblity = 255
+  }
+display(){
+  console.log(this.body.speed);
+super.display();
+
+if(this.body.speed < 3){
+  super.display();
+  
+
+  
+}
+ else{
+//do nothing ,YAY!
+World.remove(world,this.body)
+push();
+this.visblity = this.visblity - 5;
+tint(255,this.Visblity);
+image(this.image,this.body.position.x,this.body.position.y,50,50)
+pop();
+ }
+
+}
+
+};
+
+
